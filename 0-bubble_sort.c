818e6903086ac6ff6b57f09e98ad2ba6ce5c97df
Kaltouma <kchbiki363@gmail.com>
2024-01-19 19:27:40 +0100

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,7 +9,6 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp;
 	int swapped;
 
 	if (array == NULL || size < 2)
@@ -23,15 +22,15 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				array[j] = array[j] ^ array[j + 1];
+				array[j + 1] = array[j] ^ array[j + 1];
+				array[j] = array[j] ^ array[j + 1];
 
 				print_array(array, size);
 				swapped = 1;
 			}
 		}
-		if (!swapped)
+		if (swapped == 0)
 			break;
 	}
 }
