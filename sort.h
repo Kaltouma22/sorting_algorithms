b56939e6c01247a8ea9ee3f066b5a75b69f0195c
Kaltouma <kchbiki363@gmail.com>
2024-01-22 16:06:49 +0100

    selection sort

--- sort.h
+++ sort.h
@@ -24,8 +24,8 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 
 void bubble_sort(int *array, size_t size);
-
 void insertion_sort_list(listint_t **list);
-/* void swap_node(listint_t *one, listint_t *two); */
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
